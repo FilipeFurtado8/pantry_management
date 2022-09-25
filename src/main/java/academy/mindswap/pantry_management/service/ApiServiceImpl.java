e98f66c29ee96d4286d04f762a46b326d945fd9b
Filipe Furtado <filipe.furtado@mindswap.academy>
2022-09-25 19:38:59 +0100

    refactor: update get method, returns list of lists of 3 ingredients

--- src/main/java/academy/mindswap/pantry_management/service/ApiServiceImpl.java
+++ src/main/java/academy/mindswap/pantry_management/service/ApiServiceImpl.java
@@ -5,7 +5,9 @@ import academy.mindswap.pantry_management.model.Ingredient;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ApiServiceImpl implements ApiService {
@@ -14,64 +16,73 @@ public class ApiServiceImpl implements ApiService {
 
     private final IngredientService ingredientService;
 
+    private List<String> ingredients = new ArrayList<>();
+    private List<List<String>> ingredientsList = new ArrayList<>();
+    private List<String[]> arrList = new ArrayList<>();
+
     public ApiServiceImpl(ApiHandler apiHandler, IngredientService ingredientService) {
         this.apiHandler = apiHandler;
         this.ingredientService = ingredientService;
     }
 
     @Override
-    public Object getRecipesByIngredient(String ingredientName){
+    public Object getRecipesByIngredient(String ingredientName) {
         return apiHandler.getRecipesByIngredient(ingredientName);
     }
 
     @Override
-    public Object getRecipesByName(String name){
+    public Object getRecipesByName(String name) {
         return apiHandler.getRecipesByName(name);
     }
 
 
-
     @Override
-    public Object get(){
+    public Object get() {
         String[] ingredientListName = ingredientService.getAllIngredients()
                 .stream()
                 .map(Ingredient::getName)
                 .toArray(String[]::new);
+
 
         //return apiHandler.getRecipesByIngredient(ingredientListName.get(0));
 
         int rangeOfIngredients = 3;
         int stockSize = ingredientListName.length;
 
-        String[] data=new String[rangeOfIngredients];
+        String[] data = new String[rangeOfIngredients];
 
-        System.out.println(combinationUtil(ingredientListName, data, 0, stockSize - 1, 0, rangeOfIngredients));
+        return combinationUtil(ingredientListName, data, 0, stockSize - 1, 0, rangeOfIngredients);
 
         //return apiHandler.getRecipesByIngredients(combinationUtil(ingredientListName, data, 0, stockSize-1, 0, rangeOfIngredients));
 
-        return null;
 
     }
 
-    private List<String> combinationUtil(String[] ingredientListName, String[] data, int start, int end, int index,
-                                         int rangeOfIngredients) {
-        List<String> ingredients = new ArrayList<>();
+
+    private List<List<String>> combinationUtil(String[] ingredientListName, String[] data, int start, int end, int index,
+                                               int rangeOfIngredients) {
+
 
         if (index == rangeOfIngredients) {
-            for (int j=0; j<rangeOfIngredients; j++){
-                //System.out.println(data[j]+", ");
-                ingredients.add(data[j]);
-            }
-            //System.out.println("");
-            //System.out.println(ingredients);
-            return ingredients;
+
+            return arrList.stream().map(Arrays::asList).collect(Collectors.toList());
         }
 
-        for (int i=start; i<=end && end-i+1 >= rangeOfIngredients-index; i++) {
+        for (int i = start; i <= end && end - i + 1 >= rangeOfIngredients - index; i++) {
             data[index] = ingredientListName[i];
-            combinationUtil(ingredientListName, data, i+1, end, index+1, rangeOfIngredients);
+
+            combinationUtil(ingredientListName, data, i + 1, end, index + 1, rangeOfIngredients);
+
+            String[] newArr = new String[data.length];
+
+            for (int j = 0; j < data.length; j++) {
+                newArr[j] = data[j];
+            }
+
+            arrList.add(newArr);
+
         }
 
-        return null;
+        return arrList.stream().map(Arrays::asList).collect(Collectors.toList());
     }
 }
