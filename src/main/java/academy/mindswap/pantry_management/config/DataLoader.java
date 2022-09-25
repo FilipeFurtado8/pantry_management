b2e975a05d07a8c29d185b77615c47e5c8380a2a
Filipe Furtado <filipe.furtado@mindswap.academy>
2022-09-25 15:05:15 +0100

    feat: increment the size of DataLoader

--- src/main/java/academy/mindswap/pantry_management/config/DataLoader.java
+++ src/main/java/academy/mindswap/pantry_management/config/DataLoader.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Configuration
 public class DataLoader {
@@ -18,32 +19,159 @@ public class DataLoader {
                                         CategoryRepository categoryRepository) {
         return args -> {
 
-            Category category = Category.builder()
+            Category category1 = Category.builder()
                     .type("meat")
                     .build();
-            categoryRepository.save(category);
 
-            Ingredient ingredient = Ingredient.builder()
+            Category category2 = Category.builder()
+                    .type("vegetable")
+                    .build();
+
+            Category category3 = Category.builder()
+                    .type("fruit")
+                    .build();
+
+            Category category4 = Category.builder()
+                    .type("seafood")
+                    .build();
+
+            Category category5 = Category.builder()
+                    .type("dairy")
+                    .build();
+
+            Category category6 = Category.builder()
+                    .type("beverage")
+                    .build();
+
+            Category category7 = Category.builder()
+                    .type("condiments & spices")
+                    .build();
+
+            Category category8 = Category.builder()
+                    .type("bakery")
+                    .build();
+
+            Category category9 = Category.builder()
+                    .type("carbohydrates")
+                    .build();
+
+            categoryRepository.saveAll(List.of(category1, category2, category3, category4,
+                    category5, category6, category7, category8, category9));
+
+
+            Ingredient ingredient1 = Ingredient.builder()
                     .name("chicken breast")
                     .expirationDate(LocalDate.of(2022, 10, 13))
-                    .category(category)
+                    .category(category1)
                     .build();
-            ingredientRepository.save(ingredient);
 
             Ingredient ingredient2 = Ingredient.builder()
                     .name("beef")
                     .expirationDate(LocalDate.of(2022, 10, 13))
-                    .category(category)
+                    .category(category1)
                     .build();
-            ingredientRepository.save(ingredient2);
 
             Ingredient ingredient3 = Ingredient.builder()
                     .name("tomato")
                     .expirationDate(LocalDate.of(2022, 10, 13))
-                    .category(category)
+                    .category(category3)
                     .build();
-            ingredientRepository.save(ingredient3);
 
+
+            Ingredient ingredient4 = Ingredient.builder()
+                    .name("water")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category6)
+                    .build();
+
+            Ingredient ingredient5 = Ingredient.builder()
+                    .name("garlic")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category2)
+                    .build();
+
+            Ingredient ingredient6 = Ingredient.builder()
+                    .name("sugar")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category7)
+                    .build();
+
+            Ingredient ingredient7 = Ingredient.builder()
+                    .name("rice")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category9)
+                    .build();
+
+            Ingredient ingredient8 = Ingredient.builder()
+                    .name("salmon")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category4)
+                    .build();
+
+            Ingredient ingredient9 = Ingredient.builder()
+                    .name("shrimp")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category4)
+                    .build();
+
+            Ingredient ingredient10 = Ingredient.builder()
+                    .name("spaghetti")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category9)
+                    .build();
+
+            Ingredient ingredient11 = Ingredient.builder()
+                    .name("bread")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category9)
+                    .build();
+
+            Ingredient ingredient12 = Ingredient.builder()
+                    .name("potato")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category9)
+                    .build();
+
+            Ingredient ingredient13 = Ingredient.builder()
+                    .name("banana")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category3)
+                    .build();
+
+            Ingredient ingredient14 = Ingredient.builder()
+                    .name("onion")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category2)
+                    .build();
+
+            Ingredient ingredient15 = Ingredient.builder()
+                    .name("broccoli")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category2)
+                    .build();
+
+            Ingredient ingredient16 = Ingredient.builder()
+                    .name("pork")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category1)
+                    .build();
+
+            Ingredient ingredient17 = Ingredient.builder()
+                    .name("milk")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category5)
+                    .build();
+
+            Ingredient ingredient18 = Ingredient.builder()
+                    .name("cheese")
+                    .expirationDate(LocalDate.of(2022, 10, 13))
+                    .category(category5)
+                    .build();
+
+            ingredientRepository.saveAll(List.of(ingredient1, ingredient2, ingredient3, ingredient4,
+                    ingredient5, ingredient6, ingredient7, ingredient8,
+                    ingredient9, ingredient10, ingredient11, ingredient12,
+                    ingredient13, ingredient14, ingredient15, ingredient16, ingredient17, ingredient18));
         };
 
     }
